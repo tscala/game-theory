5a0077ad08497796501a5ac2e021513999af3373
tscala <tscala0@gmail.com>
2015-11-19 21:04:30 +0000

    Added naive testing.

---
+++ game-theory-domain/src/test/scala/uk/co/tscala/gametheory/domain/GameTest.scala
@@ -0,0 +1,20 @@
+package uk.co.tscala.gametheory.domain
+
+import org.scalatest._
+
+/**
+  * @author tscala
+  * @since 19/11/15.
+  */
+class GameTest extends UnitSpec {
+
+  "A game" should "contain an ID field" in {
+    val game = new Game(0, "", 0.0, 0.0)
+    assert(game.gameId !== null)
+  }
+}
+
+/**
+  * Simple Spec for unit testing.
+  */
+abstract class UnitSpec extends FlatSpec with Matchers with OptionValues with Inside
